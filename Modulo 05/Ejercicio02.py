94baff85dd6bae3de30e45423638b80e6774a0ab
JonathanGarcia15 <99307111+JonathanGarcia15@users.noreply.github.com>
2022-02-13 20:05:32 -0600

    Add files via upload

---
+++ Modulo 05/Ejercicio02.py
@@ -0,0 +1,6 @@
+primerPlaneta = input("Introduzca la distancia al sol del primer planeta: ")
+segundoPlaneta = input("Introduzca la distancia al sol del segundo planeta: ")
+
+print (f"La distancia entre planetas es: {int(primerPlaneta) - int(primerPlaneta)} km")
+
+print (f"La distancia entre planetas es: {abs((int(primerPlaneta) - int(primerPlaneta))*0.621)} millas")
